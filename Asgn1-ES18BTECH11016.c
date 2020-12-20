53367e78a96abd28ab94a56fd31a979fc03018c9
tnfssc <tnfssc@gmail.com>
2020-12-20 14:25:57 +0530

    Child creation done

--- Asgn1-ES18BTECH11016.c
+++ Asgn1-ES18BTECH11016.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <sys/shm.h>
+#include <fcntl.h>
 
-int main () {
-  pid_t pid;
-  printf("Cool");
+int main(int argc, char **argv)
+{
+  if (argc < 2 || argc > 2)
+  {
+    printf("Invalid arguments! Look at README for instructions.\n");
+    _exit(1);
+  }
+  // int fd = shm_open("memory", O_CREAT | O_RDWR, 0666);
+  pid_t pid = fork();
+  if (pid == 0)
+  {
+    execlp(argv[1], "", NULL);
+    printf("child~");
+  }
+  else if (pid > 0)
+  {
+    wait(NULL);
+    printf("parent~");
+  }
+  else
+  {
+    printf("Some error occured!");
+    _exit(1);
+  }
+  printf("\nover\n");
 }
--- Asgn1-README.txt
+++ Asgn1-README.txt
@@ -9,5 +9,6 @@ This program creates a child process. It calculates the time it took for the chi
 
 ---
 ## Usage
-- Compile using `gcc index.cpp -o time`
-- Execute the program using `./time <command>` e.g. `./time ls`
+- Compile using `gcc index.cpp`
+- Execute the program using `./a.out <command>` e.g. `./a.out ls`
+- Note that ./a.out only accepts one command. i.e. `./a.out ls` works, while `./a.out ls -l` and `./a.out "ls -l"` do not work
